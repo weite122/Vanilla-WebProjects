e186a9ef5cd1c6665f4a2c1b0cab9af4933a96f5
weite122 <949193531@qq.com>
2020-09-10 10:28:09 +0800

    feat: 添加checkRequired, checkLength, checkEmail

--- Form-Validator/index.js
+++ Form-Validator/index.js
@@ -4,7 +4,7 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const confirmPassword = document.getElementById('confirmPassword')
 
-function isValidEmail(input) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
@@ -37,6 +37,22 @@ function checkRequired(inputArr) {
   })
 }
 
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+  } else {
+    showSuccess(input)
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  }
+}
+
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1)
 }
@@ -44,4 +60,8 @@ function getFieldName(input) {
 form.addEventListener('submit', function (e) {
   e.preventDefault()
   checkRequired([username, email, password, confirmPassword])
+  checkLength(username, 3, 15)
+  checkLength(password, 6, 25)
+  checkEmail(email)
+  checkPasswordsMatch(password, confirmPassword)
 })

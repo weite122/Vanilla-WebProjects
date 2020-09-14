1c13c48b2720e9075758bb52ecb2e47b24796480
weite122 <weite122@foxmail.com>
2020-09-15 00:07:01 +0800

    feat: 实现map,sort,filter,reduce对数据的操作

--- DOM-Array-Methods/script.js
+++ DOM-Array-Methods/script.js
@@ -11,6 +11,34 @@ getRandomUser()
 getRandomUser()
 getRandomUser()
 
+function doubleMoney() {
+  data = data.map(user => {
+    return {...user, money: user.money * 2}
+  })
+
+  updateDOM()
+}
+
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money)
+  updateDOM()
+}
+
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000)
+  updateDOM()
+}
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => {
+    return acc += user.money
+  }, 0)
+  console.log(wealth)
+  const wealthEl = document.createElement('div')
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`
+  main.appendChild(wealthEl)
+}
+
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api')
   const data = await res.json()
@@ -46,4 +74,9 @@ function formatMoney(number) {
 
 
 addUserBtn.addEventListener('click', getRandomUser)
+doubleBtn.addEventListener('click', doubleMoney)
+sortBtn.addEventListener('click', sortByRichest)
+showMillionairesBtn.addEventListener('click', showMillionaires)
+calculateWealthBtn.addEventListener('click', calculateWealth)
+
 
